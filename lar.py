cfe9d753c3320eeb96a7245991e0af6231f7c534
saipreeti1999 <48148341+saipreeti1999@users.noreply.github.com>
2019-08-02 14:42:08 +0530

    Create lar.py

---
+++ lar.py
@@ -0,0 +1,4 @@
+a=int(input("enter a num:"))
+b=int(input("enter a num:"))
+c=int(input("enter a num:"))
+print("a is greater")if a>b and a>c else print("c is greater")if c>b else print("b is greater")
